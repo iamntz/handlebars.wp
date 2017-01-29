94137ffc850a10692deca76811819a15c62ee03f
Ionut Staicu <github@iamntz.com>
2017-01-29 10:03:17 +0200

    Added custom tokenizer

---
+++ src/tokenizer/CustomTokenizer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Custom Handlebars Tokenizer to allow custom template tags
+ */
+
+namespace iamntz\handlebars\helpers;
+
+/**
+ * Custom Handlebars Tokenizer
+ */
+class CustomTokenizer extends \Handlebars\Tokenizer
+{
+	protected function reset()
+	{
+		parent::reset();
+		$this->otag = '<%';
+		$this->ctag = '%>';
+	}
+}
