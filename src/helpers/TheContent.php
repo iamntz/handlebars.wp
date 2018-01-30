748a268628d690364aabe5a6c0b88c434623c10d
Ionut Staicu <github@iamntz.com>
2018-01-30 18:56:33 +0200

    tweaked the content

--- src/helpers/TheContent.php
+++ src/helpers/TheContent.php
@@ -17,14 +17,35 @@ class TheContent implements \Handlebars\Helper
 
 		$attrs = array_map([$context, 'get'], $parsed_args);
 
-		$content = wptexturize($attrs[0]);
-		// $content = convert_smilies($content);
-		$content = wpautop($content);
-		$content = shortcode_unautop($content);
-		// $content = prepend_attachment($content);
-		// $content = wp_make_content_images_responsive($content);
+		$content = $attrs[0];
+
+		if (empty($namedArgs['no-texturize'])) {
+			$content = wptexturize($content);
+		}
+
+		if (empty($namedArgs['no-smiles'])) {
+			$content = convert_smilies($content);
+		}
+
+		if (empty($namedArgs['no-autop'])) {
+			$content = wpautop($content);
+		}
+
+		if (empty($namedArgs['no-shortcodes'])) {
+			$content = shortcode_unautop($content);
+			$content = do_shortcode($content);
+		}
+
+		if (empty($namedArgs['no-prepend-attachment'])) {
+			$content = prepend_attachment($content);
+		}
+
+		if (empty($namedArgs['no-responsive-images'])) {
+			$content = wp_make_content_images_responsive($content);
+		}
 
 		$content = apply_filters('iamntz/handlebars/the_content', $content);
+
 		return str_replace(']]>', ']]&gt;', $content);
 	}
 }
