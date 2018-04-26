d111e7cbc1a8661ab0241d44cd7aff6ae07e0315
Ionuț Staicu <github@iamntz.com>
2018-04-26 10:28:55 +0300

    added extra hooks & post meta

--- src/utils/Post.php
+++ src/utils/Post.php
@@ -16,18 +16,21 @@ class Post
 	private $_excerptReadMore;
 	private $_excerptLength;
 
-	public function __construct($postID = null)
+	public function __construct($postID = null, $withContent = true)
 	{
 		$this->postID = $postID;
 		$this->post = get_post($this->postID, 'OBJECT', 'display');
 
-		$this->post->content = apply_filters('the_content', $this->post->post_content);
-		$this->post->content = str_replace(']]>', ']]&gt;', $this->post->content);
+		if ($withContent) {
+			$this->post->content = apply_filters('the_content', $this->post->post_content);
+			$this->post->content = str_replace(']]>', ']]&gt;', $this->post->content);
+		}
 
 		$this->post->title = get_the_title($this->post);
 		$this->post->permalink = get_permalink($this->post);
 
 		$this->post = apply_filters('iamntz/wp/post', $this->post);
+		$this->post = apply_filters("iamntz/wp/post/type={$this->post->post_type}", $this->post);
 	}
 
 	public function get()
@@ -40,15 +43,15 @@ class Post
 		$this->_excerptReadMore = $readMore;
 		$this->_excerptLength = $excerptLength;
 
-		add_filter( 'excerpt_more', [ $this, '_excerptReadMore' ], 20 );
+		add_filter('excerpt_more', [$this, '_excerptReadMore'], 20);
 		if ($this->_excerptLength) {
-			add_filter( 'excerpt_length', [ $this, '_excerptLength' ], 20 );
+			add_filter('excerpt_length', [$this, '_excerptLength'], 20);
 		}
 
 		$this->post->excerpt = apply_filters('the_excerpt', get_the_excerpt());
 
-		remove_filter( 'excerpt_length', [ $this, '_excerptLength' ], 20 );
-		remove_filter( 'excerpt_more', [ $this, '_excerptReadMore' ], 20 );
+		remove_filter('excerpt_length', [$this, '_excerptLength'], 20);
+		remove_filter('excerpt_more', [$this, '_excerptReadMore'], 20);
 
 		return $this;
 	}
@@ -58,7 +61,6 @@ class Post
 		return $this->_excerptReadMore;
 	}
 
-
 	public function _excerptLength()
 	{
 		return $this->_excerptLength;
@@ -66,10 +68,12 @@ class Post
 
 	public function withDate($format = null)
 	{
-		$this->post->date = apply_filters('iamntz/wp/post-date', [
+		$date = apply_filters('iamntz/wp/post-date', [
 			'iso' => get_the_date('c', $this->post),
 			'display' => get_the_date($format, $this->post),
 		], $this->post);
+
+		$this->post->date = apply_filters("iamntz/wp/post-date/type={$this->post->post_type}", $date);
 
 		return $this;
 	}
@@ -85,7 +89,7 @@ class Post
 		}
 
 		$attrs = [
-			'class' => $className
+			'class' => $className,
 		];
 
 		$thumb = [
@@ -97,7 +101,8 @@ class Post
 			'html' => wp_get_attachment_image($thumbID, $size, false, $attrs),
 		];
 
-		$this->post->thumbnail = apply_filters('iamntz/wp/post-thumbnail', $thumb, $this->post);
+		$thumb = apply_filters('iamntz/wp/post-thumbnail', $thumb, $this->post);
+		$this->post->thumbnail = apply_filters('iamntz/wp/post-thumbnail/type={$this->post->post_type}', $thumb, $this->post);
 
 		return $this;
 	}
@@ -105,6 +110,7 @@ class Post
 	public function withPostClass($postClass = '')
 	{
 		$postClass = apply_filters('iamntz/wp/post-class', $postClass, $this->post);
+		$postClass = apply_filters('iamntz/wp/post-class/type={$this->post->post_type}', $postClass, $this->post);
 		$this->post->post_class = implode(' ', get_post_class($postClass, $this->post));
 
 		return $this;
@@ -126,7 +132,8 @@ class Post
 		$cleanAuthor['permalink'] = get_author_posts_url($author->ID, $cleanAuthor['user_nicename']);
 		$cleanAuthor['description'] = get_user_meta($author->ID, 'description', true);
 
-		$this->post->author = apply_filters('iamntz/wp/post-author', $cleanAuthor, $this->post);
+		$cleanAuthor = apply_filters('iamntz/wp/post-author', $cleanAuthor, $this->post);
+		$this->post->author = apply_filters('iamntz/wp/post-author/type={$this->post->post_type}', $cleanAuthor, $this->post);
 
 		return $this;
 	}
@@ -138,15 +145,15 @@ class Post
 		}
 
 		if (is_string($key)) {
-			$this->_setAuthorMeta($key, $single);
+			$this->_setAuthorMeta($key, 0, $single);
 		} elseif (is_array($key)) {
-			array_walk($key, [$this, '_setAuthorMeta']);
+			array_walk($key, [$this, '_setAuthorMeta'], $single);
 		}
 
 		return $this;
 	}
 
-	public function _setAuthorMeta($key, $single = true)
+	public function _setAuthorMeta($key, $index, $single = true)
 	{
 		$meta = empty($this->post->author['meta']) ? [] : $this->post->author['meta'];
 
@@ -155,6 +162,28 @@ class Post
 		}
 
 		$this->post->author['meta'] = $meta;
+	}
+
+	public function withMeta($key, $single = true)
+	{
+		if (is_string($key)) {
+			$this->_setPostMeta($key, 0, $single);
+		} elseif (is_array($key)) {
+			array_walk($key, [$this, '_setPostMeta'], $single);
+		}
+
+		return $this;
+	}
+
+	public function _setPostMeta($key, $index, $single = true)
+	{
+		$meta = empty($this->post->meta) ? [] : $this->post->meta;
+
+		if (empty($meta[$key])) {
+			$meta[$key] = get_post_meta($this->post->ID, $key, $single);
+		}
+
+		$this->post->meta = $meta;
 	}
 
 	public function withTerms($taxonomy, $args = [])
@@ -168,7 +197,8 @@ class Post
 		$terms = empty($this->post->terms) ? [] : $this->post->terms;
 		$terms[$taxonomy] = $postTerms;
 
-		$this->post->terms = apply_filters('iamntz/wp/post-terms', $terms, $this->post);
+		$terms = apply_filters('iamntz/wp/post-terms', $terms, $this->post);
+		$this->post->terms = apply_filters('iamntz/wp/post-terms/type={$this->post->post_type}', $terms, $this->post);
 
 		return $this;
 	}
